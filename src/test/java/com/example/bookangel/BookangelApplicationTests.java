232b3d890bfbf84bc43f6692afb1a802d764f0f6
shin <moonjoker94@gmail.com>
2021-10-11 16:47:36 +0900

    기북천사 처음 커밋

---
+++ src/test/java/com/example/bookangel/BookangelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookangel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookangelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
