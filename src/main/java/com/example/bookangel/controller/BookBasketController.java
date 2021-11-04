73e8f1c5d7b1245106e57164ad80bf784215cd19
shin <iszzon@gmail.com>
2021-11-04 11:38:16 +0900

    책가방 담기 후 마이페이지로 이동

--- src/main/java/com/example/bookangel/controller/BookBasketController.java
+++ src/main/java/com/example/bookangel/controller/BookBasketController.java
@@ -3,7 +3,11 @@ package com.example.bookangel.controller;
 import com.example.bookangel.beans.vo.BookVO;
 import com.example.bookangel.beans.vo.Criteria;
 import com.example.bookangel.beans.vo.PageDTO;
+import com.example.bookangel.beans.vo.PaymentVO;
 import com.example.bookangel.services.BookBasketService;
+import com.example.bookangel.services.CouponService;
+import com.example.bookangel.services.MemberService;
+import com.example.bookangel.services.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -21,6 +25,9 @@ import java.util.List;
 public class BookBasketController {
 
     private final BookBasketService bookBasketService;
+    private final PaymentService paymentService;
+    private final CouponService couponService;
+    private final MemberService memberService;
 
     // main에서 책담을때 사용하는것
     @PostMapping("addBookBasket")
@@ -64,9 +71,30 @@ public class BookBasketController {
         } else { // 책가방에 담겨있음
             model.addAttribute("result","fail");
         }
-        String referer = request.getHeader("Referer");
 
-        return "redirect"+referer;
+
+        if(session.getAttribute("sub").equals("true")) {
+            PaymentVO paymentVO = paymentService.searchPayment((Long) session.getAttribute("memberNum"));
+            model.addAttribute("startSub", paymentVO.getSubDate().substring(0,10));
+            model.addAttribute("endSub", paymentVO.getExpireDate().substring(0,10));
+            model.addAttribute("end",paymentVO.getSubMonth());
+        }
+        String memberId = (String)session.getAttribute("memberId");
+        model.addAttribute("memberNum",session.getAttribute("memberNum"));
+        model.addAttribute("memberType", session.getAttribute("memberType"));
+        model.addAttribute("memberId", memberId);
+        model.addAttribute("memberName", session.getAttribute("memberName"));
+        model.addAttribute("sub", session.getAttribute("sub"));
+        model.addAttribute("myCouponCnt", couponService.companyCouponListCNT((Long)session.getAttribute("memberNum")));
+        log.info("쿠폰"+couponService.companyCouponListCNT((Long)session.getAttribute("memberNum")));
+        model.addAttribute("myInfo", memberService.getMyInfo(memberId));
+        if(couponService.companyCouponListCNT((Long)session.getAttribute("memberNum"))!=0){
+            model.addAttribute("coupons",couponService.companyCouponList((Long)session.getAttribute("memberNum")));
+        }else{
+            model.addAttribute("coupons", "null");
+        }
+
+        return "member/mypage";
         /*return "member/mypage";*/
     }
 
